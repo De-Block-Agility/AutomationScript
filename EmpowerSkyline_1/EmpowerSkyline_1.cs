5365234c96458dc2e45108c68a80b3fb20c8cfc6
Bram De Block <bram.de.block@skyline.be>
2023-03-28 16:26:01 +0200

    order dispath code

--- EmpowerSkyline_1/EmpowerSkyline_1.cs
+++ EmpowerSkyline_1/EmpowerSkyline_1.cs
@@ -55,6 +55,7 @@ namespace EmpowerSkyline_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room4;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -69,14 +70,17 @@ namespace EmpowerSkyline_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			var dms = engine.GetDms();
+			//var dms = engine.GetDms();
 
-			var elements = dms.GetElements();
+			// var elements = dms.GetElements();
 
-			foreach (var element in elements)
-			{
-				engine.GenerateInformation("element: " + element.Name);
-			}
+			//foreach (var element in elements)
+			//{
+			//	engine.GenerateInformation("element: " + element.Name);
+			//}
+
+			var order = OrderFactory.CreateOrder("Order 1");
+			order.Dispatch("Bram De Block - Agile bane of Jan Staelens");
 		}
 	}
 }
